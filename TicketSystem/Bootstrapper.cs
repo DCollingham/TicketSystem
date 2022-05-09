52f2cabdaed9d6cc113f1e3841b2e5f5006891d5
NiceLuck <40005211@petroc.ac.uk>
2022-05-09 10:03:45 +0100

    Basic Caliburn Folder Layout

---
+++ TicketSystem/Bootstrapper.cs
@@ -0,0 +1,27 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using TicketSystem.ViewModels;
+
+namespace TicketSystem
+{   //Starting Point for application
+    public class Bootstrapper : BootstrapperBase
+    {
+        //Starts bootstrapper proccesses
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+        //Overides base implementation of startup
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            //Display root for for this view model
+            DisplayRootViewFor<ShellViewModel>();
+        }
+    }
+}
